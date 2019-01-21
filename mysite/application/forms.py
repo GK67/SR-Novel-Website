e70565d824ceae0e3f6e26e18f1943e33a82b154
GK67 <shipeng123@gmail.com>
2019-01-21 17:27:15 -0500

    work on sign up page, unable to register

--- mysite/application/forms.py
+++ mysite/application/forms.py
@@ -5,39 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 from application.models import Profile, Book, Favorite, Marker, Author, Genre
 from django.core.exceptions import ValidationError
 
-class CustomUserCreationForm(forms.Form):
-    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
-    email = forms.EmailField(label='Enter email')
-    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
- 
-    def clean_username(self):
-        username = self.cleaned_data['username'].lower()
-        r = User.objects.filter(username=username)
-        if r.count():
-            raise  ValidationError("Username already exists")
-        return username
- 
-    def clean_email(self):
-        email = self.cleaned_data['email'].lower()
-        r = User.objects.filter(email=email)
-        if r.count():
-            raise  ValidationError("Email already exists")
-        return email
- 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
- 
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Password don't match")
- 
-        return password2
- 
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password1']
-        )
-        return user
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta: 
+        model = User
+        fields = ('username', 'first_name','last_name','email','password1','password2')
--- mysite/application/views.py
+++ mysite/application/views.py
@@ -1,27 +1,32 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from application.models import Profile, Book, Favorite, Marker, Author, Genre
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
-
-
-from .forms import CustomUserCreationForm
- 
-#...
+from django.contrib.auth import login, authenticate
+from .forms import RegistrationForm
 def register(request):
-    if request.method == 'get':
-        f = CustomUserCreationForm(request.POST)
+    if request.method == 'GET':
+        f = RegistrationForm(request.GET)
         if f.is_valid():
             f.save()
-            messages.success(request, 'Account created successfully')
-            return redirect('index')
- 
+            username=f.cleaned_data.get('username')
+            raw_password=f.cleaned_data.get('password2')
+            
+            
+            user = authenticate(username=username,password=raw_password)
+            login(request,user)        
+            return redirect('/')
+
     else:
-        f = CustomUserCreationForm()
- 
-    return render(request, '/signup', {'form': f})
+        f = RegistrationForm()
+
+        return render(request, 'SignUp.html', {'form': f}) 
+
+
+    
 # Create your views here.
 def index(request):
 	context = {
@@ -40,3 +45,16 @@ class SignUpView(generic.TemplateView):
         context = super(SignUpView, self).get_context_data(**kwargs)
         return context
 
+    def register(request):
+        if request.method == 'GET':
+            f = CustomUserCreationForm(request.GET)
+            if f.is_valid():
+                f.save()
+                messages.success(request, 'Account created successfully')
+            
+                return redirect('/index')
+ 
+        else:
+            f = CustomUserCreationForm()
+ 
+        return render(request, 'index.html', {'form': f})
