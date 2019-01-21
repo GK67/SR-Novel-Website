0cd94132ac85af97db6a704edaba1536623d01cc
GK67 <shipeng123@gmail.com>
2019-01-21 15:53:29 -0500

    signup page update

--- mysite/application/views.py
+++ mysite/application/views.py
@@ -1,6 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from application.models import Profile, Book, Favorite, Marker, Author, Genre
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
+
+
+from .forms import CustomUserCreationForm
+ 
+#...
+def register(request):
+    if request.method == 'get':
+        f = CustomUserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('index')
+ 
+    else:
+        f = CustomUserCreationForm()
+ 
+    return render(request, '/signup', {'form': f})
 # Create your views here.
 def index(request):
 	context = {
@@ -11,4 +32,11 @@ def index(request):
 
 class LoginView(generic.TemplateView):
     template_name='login.html'
+
+class SignUpView(generic.TemplateView):
+    template_name='SignUp.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SignUpView, self).get_context_data(**kwargs)
+        return context
 
