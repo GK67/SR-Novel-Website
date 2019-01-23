26551ff1d6c0ca43f3fea762ec56518f8c3800e0
Ruifeng Zhang <ruifeng.zhang1993@gmail.com>
2019-01-22 19:40:04 -0500

    recover

--- mysite/application/forms.py
+++ mysite/application/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from application.models import Profile, Book, Marker, Author, Genre
 from django.core.exceptions import ValidationError
+
+# class RegistrationForm(UserCreationForm):
+#     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+#     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+#     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+#     class Meta: 
+#         model = User
+#         fields = ('username', 'first_name','last_name','email','password1','password2')
 
 
 from django.contrib.auth import authenticate
@@ -20,10 +29,3 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=30, required=True, help_text='Required field.')
-    password = forms.CharField(max_length=30, required=True, help_text='Required field.')
-
-    class Meta:
-        model = User
-        fields = ('username','password')
